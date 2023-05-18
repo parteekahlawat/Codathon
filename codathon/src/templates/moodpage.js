904461195382c322ef57e74b43746bfb2dc3c638
Parteek <72187356+parteekahlawat@users.noreply.github.com>
2023-05-18 17:56:40 +0530

    changes

---
+++ codathon/src/templates/moodpage.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+
+function MoodDetector() {
+    const [mood, setMood] = useState('');
+  
+    const handleMoodSelect = (selectedMood) => {
+      setMood(selectedMood);
+    };
+  
+  const emojiList = [
+    '😃', '😔', '😍', '😎', '😂',
+    '😊', '😢', '🥰', '😞', '😭', '😌', '🙃'
+  ];
+  
+    const column1 = emojiList.slice(0, emojiList.length / 3);
+    const column2 = emojiList.slice(emojiList.length / 3, (emojiList.length / 3) * 2);
+    const column3 = emojiList.slice((emojiList.length / 3) * 2);
+  
+    return (
+      <div className="container">
+        <h2>Mood Detector</h2>
+        <div className="emojis">
+          <div className="column">
+            {column1.map((emoji) => (
+              <div
+                className="emoji"
+                key={emoji}
+                onClick={() => handleMoodSelect(emoji)}
+              >
+                {emoji}
+              </div>
+            ))}
+          </div>
+          <div className="column">
+            {column2.map((emoji) => (
+              <div
+                className="emoji"
+                key={emoji}
+                onClick={() => handleMoodSelect(emoji)}
+              >
+                {emoji}
+              </div>
+            ))}
+          </div>
+          <div className="column">
+            {column3.map((emoji) => (
+              <div
+                className="emoji"
+                key={emoji}
+                onClick={() => handleMoodSelect(emoji)}
+              >
+                {emoji}
+              </div>
+            ))}
+          </div>
+        </div>
+        {mood && (
+          <div className="selected-mood">
+            You selected: <span className="mood">{mood}</span>
+          </div>
+        )}
+  
+        <style jsx>{`
+          .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            background-color: #f5f5f5;
+          }
+  
+          h2 {
+            font-size: 24px;
+            margin-bottom: 20px;
+            color: #333;
+          }
+  
+          .emojis {
+            display: flex;
+            justify-content: center;
+            margin-bottom: 20px;
+          }
+  
+          .column {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            margin: 0 20px;
+          }
+  
+          .emoji {
+            font-size: 60px;
+            margin-bottom: 10px;
+            cursor: pointer;
+            color: #333;
+            transition: transform 0.2s ease-in-out;
+          }
+  
+          .emoji:hover {
+            transform: scale(1.2);
+          }
+  
+          .selected-mood {
+            font-size: 18px;
+            margin-top: 20px;
+            color: #333;
+          }
+  
+          .mood {
+            font-size: 24px;
+            font-weight: bold;
+          }
+        `}</style>
+      </div>
+    );
+}
+  
+export default MoodDetector;  
---
+++ codathon/src/templates/signin.js
@@ -0,0 +1,166 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+function SignInPage(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Add your sign-in logic here
+    console.log('Username:', username);
+    console.log('Password:', password);
+
+    //signin redirect
+    if (username === 'Admin' && password === '12345') {
+        // Successful sign-in
+        console.log('Sign-in successful');
+        navigate("/moodpage")
+        // Add your code for redirecting to the next page
+      } else {
+        // Invalid credentials
+        setErrorMessage('Invalid username or password');
+        window.alert('Wrong Password');
+      }
+
+  };
+
+  return (
+    <div className="container">
+      <h2>{props.title}</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username" className="label">Username</label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter your username"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password" className="label">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <button type="submit">Sign In</button>
+        </div>
+        <div className="form-group">
+          <p className="message">
+            Forgot your password? <a href="#">Reset it</a>
+          </p>
+        </div>
+      </form>
+      <style jsx>{`
+      .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    background: linear-gradient(to right, #0f2027, #203a43, #2c5364);
+    background-size: cover;
+    background-position: center;
+    color: #fff;
+  }
+  
+  h2 {
+    font-size: 24px;
+    margin-bottom: 20px;
+  }
+  
+  .label {
+    color: #555;
+  }
+  
+  form {
+    width: 300px;
+    padding: 20px;
+    background-color: rgba(255, 255, 255, 0.9);
+    border-radius: 6px;
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.3);
+  }
+  
+  .form-group {
+    margin-bottom: 15px;
+  }
+  
+  label {
+    display: block;
+    font-size: 14px;
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+  
+  input[type='text'],
+  input[type='password'] {
+    width: 100%;
+    padding: 8px;
+    font-size: 14px;
+    border: none;
+    border-radius: 4px;
+    background-color: #f5f5f5;
+  }
+  
+  input[type='text']:focus,
+  input[type='password']:focus {
+    outline: none;
+    background-color: #ebebeb;
+  }
+  
+  button {
+    width: 100%;
+    padding: 8px;
+    font-size: 14px;
+    font-weight: bold;
+    color: #fff;
+    background-color: #007bff;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  
+  button:hover {
+    background-color: #0056b3;
+  }    .message {
+      font-size: 12px;
+      text-align: center;
+    }
+  
+    .message a {
+      color: #007bff;
+      text-decoration: none;
+    }
+    .error-message {
+        color: red;
+        font-size: 14px;
+        text-align: center;
+      }
+    `}
+    </style>
+    
+      </div>
+);
+}
+
+export default SignInPage;      
